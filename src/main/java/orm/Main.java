568e4806d6cce154f8da8d49544f46c44e469cd9
PiotrMakarewicz <makarepio@gmail.com>
2022-02-08 12:42:17 +0100

    Changes by pholo

--- src/main/java/orm/Main.java
+++ src/main/java/orm/Main.java
@@ -12,29 +12,34 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception {
         Session session = SessionFactory.getInstance().createSession();
+        Teacher teacher = (Teacher) session.load(Teacher.class,1);
+        System.out.println(teacher.subject.subjectName);
+        System.out.println(teacher.firstName);
+        System.out.println(teacher.lastName);
 
-        Account account = new Account();
-        account.setUserName("misiek2000");
-        account.setPassword("super-tajne-haslo");
 
-        Student student = new Student();
-        student.setFirstName("Michał");
-        student.setLastName("Studencki");
-
-        student.setAccount(account);
-        account.setStudent(student);
-
-        Professor professor = new Professor();
-        professor.setFirstName("Jan");
-        professor.setLastName("Profesorski");
-
-        professor.addStudent(student);
-        student.addProfessor(professor);
-
-        session.save(student);
-        session.save(professor);
-        session.save(account);
-        session.flush();
+//        Account account = new Account();
+//        account.setUserName("misiek2000");
+//        account.setPassword("super-tajne-haslo");
+//
+//        Student student = new Student();
+//        student.setFirstName("Michał");
+//        student.setLastName("Studencki");
+//
+//        student.setAccount(account);
+//        account.setStudent(student);
+//
+//        Professor professor = new Professor();
+//        professor.setFirstName("Jan");
+//        professor.setLastName("Profesorski");
+//
+//        professor.addStudent(student);
+//        student.addProfessor(professor);
+//
+//        session.save(student);
+//        session.save(professor);
+//        session.save(account);
+//        session.flush();
 
     }
 }
